8ac137f3e62e189f62b31d32ef153eb86a8a7354
Fausto R <fausto91@gmail.com>
2021-11-12 19:57:37 +0100

    Initial Commit

---
+++ SampleWithActorsAndResolver/SampleWithActorsAndResolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWithActorsAndResolverApp.swift
+//  SampleWithActorsAndResolver
+//
+//  Created by Fausto Ristagno on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleWithActorsAndResolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
